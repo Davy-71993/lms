4df5ee96a920f663a508454d0caefe0ad85a0b45
Davy-71993 <davy.kyute@gmail.com>
2025-03-11 15:45:55 +0300

    New demo content

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -4,16 +4,30 @@
  * The srcipt tag is commented to avoid some kind of misbehavour.
  */
 
+import Script from "next/script";
+
 export default async function EnrolmentsPage() {
   return (
     <div className="w-screen max-w-5xl mx-auto px-5 py-2">
-        <iframe src="https://koikoistories.h5p.com/content/1292496330719392027/embed" 
-            className=" w-full min-w-full"
-            aria-label="Learning on Occupational Health and Safety : The Felt Leadership Module" 
-            style={{height:"90vh", width:"80vw"}} frameBorder="0" allowFullScreen={true} 
-            allow="autoplay *; geolocation *; microphone *; camera *; midi *; encrypted-media *">
-        </iframe>
-        {/* <Script src="https://koikoistories.h5p.com/js/h5p-resizer.js"></Script> */}
+      {/* <iframe
+        src="https://koikoistories.h5p.com/content/1292496330719392027/embed"
+        className=" w-full min-w-full"
+        aria-label="Learning on Occupational Health and Safety : The Felt Leadership Module"
+        style={{ height: "90vh", width: "80vw" }}
+        frameBorder="0"
+        allowFullScreen={true}
+        allow="autoplay *; geolocation *; microphone *; camera *; midi *; encrypted-media *"
+      ></iframe> */}
+      <iframe
+        src="https://doline.h5p.com/content/1292530833092412627/embed"
+        aria-label="Martian Ventures LMS Demo App"
+        width="1088"
+        height="637"
+        allowFullScreen={true}
+        allow="autoplay *; geolocation *; microphone *; camera *; midi *; encrypted-media *"
+      ></iframe>
+      <Script src="https://doline.h5p.com/js/h5p-resizer.js"></Script>
+      {/* <Script src="https://koikoistories.h5p.com/js/h5p-resizer.js"></Script> */}
     </div>
-  )
+  );
 }
